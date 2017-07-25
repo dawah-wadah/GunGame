d15bfec7d099e97d42efad9baf8d98cfceb9d336
Wadah Adlan <wadahadlan@gmail.com>
2017-07-24 22:36:36 -0400

    added basic movements

---
+++ animations/scottAnimations.js
@@ -0,0 +1,50 @@
+export const idleAnimation = {
+  0: {
+    x: 32,
+    y: 92,
+    width: 37,
+    height: 58,
+  },
+  1: {
+    x: 73,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+  2: {
+    x: 116,
+    y: 92,
+    width: 37,
+    height: 53,
+  },
+  3: {
+    x: 156,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+  4: {
+    x: 196,
+    y: 92,
+    width: 37,
+    height: 58,
+  },
+  5: {
+    x: 236,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+  6: {
+    x: 275,
+    y: 92,
+    width: 37,
+    height: 53,
+  },
+  7: {
+    x: 317,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+};
--- gunGame.js
+++ gunGame.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
   canvas.height = 736;
   const frames = 0;
   const game = new __WEBPACK_IMPORTED_MODULE_0__game_js__["a" /* default */](canvas, ctx, frames);
-  game.gameLoop();
+  game.start();
 
   // document.addEventListener('keypress', (e) => {
   //   console.log(e.keyCode);
@@ -122,43 +122,115 @@ document.addEventListener("DOMContentLoaded", () => {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__soldier_js__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__scott_js__ = __webpack_require__(3);
+
 
 
 class Game {
   constructor(canvas, ctx, frames) {
     this.canvas = canvas;
-    this.ctx= ctx;
+    this.ctx = ctx;
     this.frames = frames;
     this.gameLoop = this.gameLoop.bind(this);
     this.soldier = new __WEBPACK_IMPORTED_MODULE_0__soldier_js__["a" /* default */](this.canvas, this.ctx, 40, 40);
+    this.scott = new __WEBPACK_IMPORTED_MODULE_1__scott_js__["a" /* default */]({
+      xPos: 20,
+      yPos: 30,
+      width: 50,
+      height: 100,
+      canvas: this.canvas,
+      ctx: this.ctx,
+      src: 'res/scott.png',
+      spritePicker: 0
+    });
+    this.keys = {
+      37: {
+        down: false,
+        action: () => this.scott.move('left')
+      },
+      38: {
+        down: false,
+        action: () => this.scott.move('up')
+      },
+      39: {
+        down: false,
+        action: () => this.scott.move('right')
+      },
+      40: {
+        down: false,
+        action: () => this.scott.move('down')
+      },
+
+      65: {
+        down: false,
+        action: () => this.scott.move('left')
+      },
+      68: {
+        down: false,
+        action: () => this.scott.move('right')
+      },
+      83: {
+        down: false,
+        action: () => this.scott.move('down')
+      },
+      87: {
+        down: false,
+        action: () => this.scott.move('up')
+      },
+    };
+    this.start = this.start.bind(this);
   }
 
-  update(){
+  update() {
+    for (var key in this.keys) {
+      if (this.keys[key].down) {
+        this.keys[key].action();
+      }
+    }
+  }
 
+  start() {
+    document.addEventListener('keydown', e => {
+      if (this.keys[e.keyCode]) {
+        this.keys[e.keyCode].down = true;
+      }
+    });
+
+    document.addEventListener('keyup', e => {
+      if (this.keys[e.keyCode]) {
+        this.keys[e.keyCode].down = false;
+        this.scott.status = 'idle';
+      }
+    });
+
+    document.addEventListener('keypress', e => {
+      if (e.keyCode === 32) {
+        this.tank.fireBullet();
+      }
+    });
+    this.gameLoop();
   }
 
 
   _collided(objA, objB) {
-  let a = objA.xPos < objB.xPos + objB.width - 5;
-  let b = objA.xPos + objA.width - 5 > objB.xPos;
-  let c = objA.yPos < objB.yPos + objB.height;
-  let d = objA.height + objA.yPos > objB.yPos;
-  if (a && b && (c && d)) {
-    return true;
+    let a = objA.xPos < objB.xPos + objB.width - 5;
+    let b = objA.xPos + objA.width - 5 > objB.xPos;
+    let c = objA.yPos < objB.yPos + objB.height;
+    let d = objA.height + objA.yPos > objB.yPos;
+    if (a && b && (c && d)) {
+      return true;
+    }
   }
-}
 
-gameLoop() {
-  this.frames++;
-  this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-  this.soldier.render();
-
-  if (this.currentState !== 'Paused') {
+  gameLoop() {
+    this.frames++;
+    // this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.scott.update();
+    this.update();
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.scott.render();
     this.gameID = window.requestAnimationFrame(this.gameLoop);
-  } else {
-    window.cancelAnimationFrame(this.gameID);
   }
-}
 }
 
 /* harmony default export */ __webpack_exports__["a"] = (Game);
@@ -193,6 +265,145 @@ class Soldier {
 }
 
 /* harmony default export */ __webpack_exports__["a"] = (Soldier);
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__moving_objects_js__ = __webpack_require__(5);
+
+
+
+
+class Scott extends __WEBPACK_IMPORTED_MODULE_1__moving_objects_js__["a" /* default */] {
+  constructor(options) {
+    super(options);
+    this.status = 'idle';
+  }
+
+  update(){
+    this.spritePicker += .2;
+  }
+  render(){
+    this.ctx.drawImage(
+      this.image,
+      __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__["a" /* idleAnimation */][Math.floor(this.spritePicker % 8)].x,
+      __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__["a" /* idleAnimation */][Math.floor(this.spritePicker % 8)].y,
+      __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__["a" /* idleAnimation */][Math.floor(this.spritePicker % 8)].width,
+      __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__["a" /* idleAnimation */][Math.floor(this.spritePicker % 8)].height,
+      this.xPos,
+      this.yPos,
+      __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__["a" /* idleAnimation */][Math.floor(this.spritePicker % 8)].width * 3,
+      __WEBPACK_IMPORTED_MODULE_0__animations_scottAnimations_js__["a" /* idleAnimation */][Math.floor(this.spritePicker % 8)].height * 3
+    );
+  }
+
+  move(direction){
+    this.status = 'moving';
+    switch (direction) {
+      case 'up':
+        this.yPos -= 1;
+        break;
+      case 'down':
+        this.yPos += 1;
+        break;
+      case 'left':
+        this.xPos -= 1;
+        break;
+      case 'right':
+        this.xPos += 1;
+        break;
+      default:
+
+    }
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Scott;
+
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const idleAnimation = {
+  0: {
+    x: 32,
+    y: 92,
+    width: 37,
+    height: 58,
+  },
+  1: {
+    x: 73,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+  2: {
+    x: 116,
+    y: 92,
+    width: 37,
+    height: 53,
+  },
+  3: {
+    x: 156,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+  4: {
+    x: 196,
+    y: 92,
+    width: 37,
+    height: 58,
+  },
+  5: {
+    x: 236,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+  6: {
+    x: 275,
+    y: 92,
+    width: 37,
+    height: 53,
+  },
+  7: {
+    x: 317,
+    y: 92,
+    width: 37,
+    height: 56,
+  },
+};
+/* harmony export (immutable) */ __webpack_exports__["a"] = idleAnimation;
+
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class MovingObject {
+  constructor(options) {
+    this.xPos = options.xPos;
+    this.yPos = options.yPos;
+    this.width = options.width;
+    this.height = options.height;
+    this.canvas = options.canvas;
+    this.ctx = options.ctx;
+    this.image = new Image();
+    this.image.src = options.src;
+    this.spritePicker = 0;
+  }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = MovingObject;
+
 
 
 /***/ })
